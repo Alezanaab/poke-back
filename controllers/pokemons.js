41a3f2346afe60f00748acd7f310a8cce51ebd03
Alexia Abrego <aabrego@unitedvirtualities.com>
2022-01-28 02:40:18 -0700

    Update Return pokemon by Name controller

--- controllers/pokemons.js
+++ controllers/pokemons.js
@@ -10,12 +10,25 @@ exports.findAll = function (req, res) {
     .then(allpokemon => res.status(200).json(allpokemon));
   };
 
+  //GET - Return pokemon by Name
   exports.findByName = function (req, res) {
     console.log('pokemon by name');
     console.log(req.params.name);
-    fetch('https://pokeapi.co/api/v2/pokemon/'+req.params.name)
-    .then(pokemon => { 
-        console.log(pokemon);
-        pokemon.size == 0 ? res.status(500).json({'status':'not found'}) : res.status(200).json(pokemon)})
-    .then(pokemon => { console.log(pokemon)});
+    fetch('https://pokeapi.co/api/v2/pokemon/'+req.params.name, {
+        headers : { 
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+          'Access-Control-Allow-Origin': '*'
+         }
+      })
+    .then(response => { console.log(response)
+        return response.json()})//response.json())
+    .then(function(pokeData){
+     console.log(pokeData);
+     res.send(pokeData).status(500);
+     })
+     .catch(err => { 
+     console.log(err);
+     res.send({'status':'not found'}).status(500);
+    })
   };
