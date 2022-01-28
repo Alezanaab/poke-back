0390c0b12ee47f57d01ddef04d6fee50684601dc
Alexia Abrego <aabrego@unitedvirtualities.com>
2022-01-28 02:41:12 -0700

    Fix cors issue

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express         = require("express"),
     bodyParser      = require("body-parser"),
     methodOverride  = require("method-override"),
     mongoose        = require('mongoose');
+var cors = require('cors')
 
 // Connection to DB
 mongoose.connect('mongodb://localhost:27017/tvshows', function(err, res) {
@@ -20,6 +21,7 @@ mongoose.connect('mongodb://localhost:27017/pokemons', function(err, res) {
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(methodOverride());
+app.use(cors());
 
 // Import Models and controllers
 var models     = require('./models/tvshow')(app, mongoose);
@@ -58,6 +60,13 @@ pokemons.route('/pokemons/:name')
 
 app.use('/api', tvshows);
 app.use('/api', pokemons);
+
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 // Start server
 app.listen(3000, function() {
